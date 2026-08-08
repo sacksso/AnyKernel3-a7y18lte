61dfc099ddbf1bed4efe446ee145aa9fb6148f3a
sacksso <147282285+sacksso@users.noreply.github.com>
2026-08-08 06:24:19 +0200

    Refactor anykernel.sh for improved configuration
    Updated AnyKernel3 script for Samsung Galaxy A7 2018 with new properties and structure.

--- anykernel.sh
+++ anykernel.sh
@@ -1,31 +1,35 @@
 #!/sbin/sh
-#
-# AnyKernel3 script - a7y18lte
-#
-# This is a standard template. Adjust as needed.
+## AnyKernel3 setup script
+# osm0sis @ xda-developers
 
-# Set up variables
-kernel=Image
+# Set properties
+properties() { '
+kernel.string=Kernel for Samsung Galaxy A7 2018 (a7y18lte) - Clean SELinux
+do.devicecheck=1
+do.modules=0
+do.systemless=0
+do.cleanup=1
+do.cleanuponabort=0
 device.name1=a7y18lte
 device.name2=SM-A750F
 device.name3=SM-A750FN
 device.name4=SM-A750G
 device.name5=
-device.name6=
+supported.versions=
+supported.patchlevels=
+'; }
 
-# Set up paths
-block=/dev/block/platform/13500000.dwmmc0/by-name/BOOT
-is_slot_device=0
-ramdisk_compression=auto
+# Set shell variables
+block=boot;
+is_slot_device=auto;
+ramdisk_compression=auto;
 
-# Set up functions
-. /tmp/anykernel/tools/ak3-core.sh
+# Import the core script
+. tools/ak3-core.sh;
 
-# Dump current boot image for modification
+# Custom modifications (if any)
+# You can add your own tweaks here
+
+# Flash the boot image
 dump_boot;
-
-# Write the kernel (Image) to the boot image
 write_boot;
-
-# Done
-reset_ak;
